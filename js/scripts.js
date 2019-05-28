be61ed076c0dbec748d12774ed4da2e1dc5bb926
Matkava Victoria <Matkava.V.Vl@sberbank.ru>
2019-05-28 15:00:13 +0300

    npm scripts

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+				$('#mycarousel').carousel({ interval: 2000 });
+				$("#carouselButton").click(function(){
+                	if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    	$("#mycarousel").carousel('pause');
+                    	$("#carouselButton").children("span").removeClass('fa-pause');
+                    	$("#carouselButton").children("span").addClass('fa-play');
+                	}
+                	else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    	$("#mycarousel").carousel('cycle');
+                    	$("#carouselButton").children("span").removeClass('fa-play');
+                    	$("#carouselButton").children("span").addClass('fa-pause');                    
+                	}
+            	});
+
+				$("#reserveButton").click( function(){
+					
+					$("#loginModal").modal('hide');
+					
+					$("#reserveModal").modal('show');
+				});
+
+				$("#loginButton").click( function(){
+						$("#reserveModal").modal('hide');
+					
+					$("#loginModal").modal('show');
+				})
+				
+			});
